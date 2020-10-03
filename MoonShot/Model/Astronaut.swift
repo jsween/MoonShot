ce66f3ea3d46f2dbca33c0260f5126d33dca3c94
Jon Sweeney <jonsweeney26@gmil.com>
2020-10-03 13:11:21 -0700

    Initial Commit

---
+++ MoonShot/Model/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Jonathan Sweeney on 10/3/20.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String 
+}
---
+++ MoonShot/ViewModel/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Jonathan Sweeney on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
